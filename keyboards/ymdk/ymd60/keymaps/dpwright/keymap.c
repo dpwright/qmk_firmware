be2a97feebc83646e055c00b206ac95f500e9a3e
Daniel P. Wright <dani@dpwright.com>
2020-10-21 07:56:00 -0700

    YMDK60: Add F13 key and make some whitespace changes

--- keyboards/ymdk/ymd60/keymaps/dpwright/keymap.c
+++ keyboards/ymdk/ymd60/keymaps/dpwright/keymap.c
@@ -9,14 +9,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,   JP_Q,    JP_W,    JP_E,     JP_R,   JP_T,     JP_Y,    JP_U,    JP_I,     JP_O,    JP_P,    JP_AT,   JP_LBRC, XXXXXXX,
 		KC_LCTRL, JP_A,    JP_S,    JP_D,     JP_F,   JP_G,     JP_H,    JP_J,    JP_K,     JP_L,    JP_SCLN, JP_COLN, JP_RBRC,          KC_ENT,
 		KC_LSFT,  XXXXXXX, JP_Z,    JP_X,     JP_C,   JP_V,     JP_B,    JP_N,    JP_M,     JP_COMM, JP_DOT,  JP_SLSH, JP_BSLS, KC_UP,   KC_RSFT,
-		MO(1),             KC_LALT, KC_LGUI,  LT(1, JP_MEISU),  KC_SPC,  LT(1, JP_MKANA),   KC_RGUI,          KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT),
+		MO(1),             KC_LALT, KC_LGUI,  LT(1, JP_MEISU),  KC_SPC,  LT(1, JP_MKANA),   KC_RGUI, KC_RALT,          KC_LEFT, KC_DOWN, KC_RGHT),
 
 	KEYMAP(
-		RESET,   KC_F1,       KC_F2,     KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
+		RESET,   KC_F1,       KC_F2,     KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_F13,  KC_DEL,
 		_______, RGB_TOG,     _______,   RGB_MOD,  RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,  _______, _______, _______, _______,
 		_______, KC__VOLDOWN, KC__VOLUP, KC__MUTE, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, _______,          _______,
 		_______, _______,     _______,   _______,  BL_DEC,  BL_TOGG, BL_INC,  _______, _______, _______,  _______, _______, _______, KC_PGUP, _______,
-		_______,              _______,   _______,  _______,          _______,          _______, _______,           _______, _______, KC_PGDN, _______),
+		_______,              _______,   _______,  _______,          _______,          _______, _______,  _______,          _______, KC_PGDN, _______),
 
 	KEYMAP(
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
